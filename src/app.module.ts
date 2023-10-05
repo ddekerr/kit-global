b4e8ac3981e61883d735f4d44dde72adee98cf96
ddekerr <dimon.bond.94@gmail.com>
2023-10-05 13:57:58 +0300

    connect to Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_CONECTION),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
